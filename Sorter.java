292fa50dbaf402eaa5ebfdb398f40925ff0121e1
unknown <HasanMajed27@hotmail.com>
2023-09-19 21:09:10 +0300

    .

--- Sorter.java
+++ Sorter.java
@@ -1,3 +1,17 @@
 public class Sorter {
-    
+    public static void selectionSort(int[] arr){
+        int min = 0; // index of the minimum.
+        for(int i = 0; i < arr.length; i++){
+        min = i;
+        for(int j = i; j < arr.length; j++){
+        if(arr[min] > arr[j]){
+        min = j;
+        }
+        }
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+        }
+        
+    }
 }
